e61194da977b16979883218323f1daaaf905662a
Issac Saji <issac@sayonetech.com>
2017-01-25 10:42:59 +0530

    new user model and migrations

--- oscarapps/customer/models.py
+++ oscarapps/customer/models.py
@@ -1,14 +1,14 @@
-from oscar.apps.customer.models import *
-from oscar.apps.customer.abstract_models import *
-from django.contrib.auth.models import User
-from oscar.apps.customer.abstract_models import AbstractUser
+# from django.contrib.auth.models import User
+# from oscar.apps.customer.abstract_models import AbstractUser
 from django.db import models
-from oscarapps.catalogue.models import *
+# from oscarapps.catalogue.models import *
 from django.utils.translation import gettext as _
+from django.conf import settings
+
 
 
 class UserProductLike(models.Model):
-    user = models.ForeignKey( User )
+    user = models.ForeignKey( settings.AUTH_USER_MODEL )
     product_like = models.ForeignKey( 'catalogue.Product' )
 
     class Meta:
@@ -17,6 +17,19 @@ class UserProductLike(models.Model):
 
     def __str__(self):
         return self.product_like.likes
+
+class UserVerify(models.Model):
+    customer = models.ForeignKey( settings.AUTH_USER_MODEL )
+    verification_code = models.TextField(null=False,blank=False, max_length=11)
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = ('User Verification')
+
+    def __str__(self):
+        return self.customer.email
+
 
 
 from oscar.apps.customer.models import *
---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from oscar.apps.customer.abstract_models import AbstractUser
+
+
+class User(AbstractUser):
+
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.last_name.upper(), self.first_name)
+        return full_name.strip()
