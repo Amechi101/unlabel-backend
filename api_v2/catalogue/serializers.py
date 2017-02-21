dffc3b173d74a13e4e85fd65f0ffc1ace3f91186
Issac Saji <issac@sayonetech.com>
2017-02-21 19:41:38 +0530

    size issue

--- api_v2/catalogue/serializers.py
+++ api_v2/catalogue/serializers.py
@@ -8,7 +8,7 @@ from oscarapi.utils import (
 
 from oscarapps.partner.models import Partner, Style, Category, PartnerFollow, RentalInformation, StockRecord
 from oscarapps.address.models import Locations, States
-from oscarapps.catalogue.models import Product, Size, InfluencerProductImage
+from oscarapps.catalogue.models import Product, InfluencerProductImage
 from oscarapps.influencers.models import Influencers, InfluencerProductReserve
 from oscar.core.loading import get_model, get_class
 
@@ -192,12 +192,6 @@ class StoreTypeSerializer(OscarModelSerializer):
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = States
-        fields = '__all__'
-
-
-class SizeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Size
         fields = '__all__'
 
 
