3f8b397340bbc6a84686abc927a5c405fd0ff9e0
Stéphane Guigné <stephane.guigne@gmail.com>
2017-06-16 14:51:18 +0200

    even better nested lists

--- static/unlabel/src/js/components/nested-lists.js
+++ static/unlabel/src/js/components/nested-lists.js
@@ -1,14 +1,13 @@
-import {TweenLite, Power3} from 'gsap'
-import closest from 'closest'
 import Collapsible from './collapsible'
+import closest from 'closest'
 
 class NestedLists {
   constructor({el, options = {}}) {    
     this.el = el
     this.options = {
       listSelector: '.nested-lists__list',
-      togglerSelector: '.nested-lists__link',
-      related: false // TODO : add behavior
+      parentSelector: '.nested-lists__item',
+      togglerSelector: '.nested-lists__link'
     }
 
     Object.keys(this.options).forEach((key) => {
@@ -24,12 +23,12 @@ class NestedLists {
     this.lists_arr = [].slice.call(this.el.querySelectorAll(this.options.listSelector))
 
     this.lists_arr.forEach((list_el) => {
-      const nested_list = list_el.querySelector(this.options.listSelector)
-      if( nested_list ){
-        const nested_list_toggler = list_el.querySelector(this.options.togglerSelector)
-        const collapsible_ctrl = new Collapsible({
-          el: nested_list,
-          toggler_el: nested_list_toggler
+      const parent_el = closest(list_el, this.options.parentSelector)
+      if( parent_el ){
+        const list_toggler = parent_el.querySelector(this.options.togglerSelector)
+        new Collapsible({
+          el: list_el,
+          toggler_el: list_toggler
         })
       }
     })
